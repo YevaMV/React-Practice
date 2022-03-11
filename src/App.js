2a265990457a8e147623f2c3d94f58578f9a1c8b
YevaMV <eva280988@gmail.com>
2022-03-11 18:29:01 +0400

    added Users List Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddUsers from './components/Users/AddUsers';
+import USersList from './components/Users/UsersList';
 
 function App() {
   return (
     <div>
       <AddUsers />
+      <USersList users={[]} />
     </div>
   );
 }
---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,18 @@
+import classes from './UsersList.module.css';
+import Card from '../UI/Card';
+
+function USersList(props) {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+export default USersList;
